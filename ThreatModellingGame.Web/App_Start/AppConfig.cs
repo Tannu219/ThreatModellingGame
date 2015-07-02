f1e74b4eb68650dbef8aef61b12957aa8cc4cc43
Dustin Moris Gorski <dustinmoris@gmail.com>
2015-07-02 11:52:13 +0100

    Implemented basic Bootstrap theme

--- ThreatModellingGame.Web/App_Start/AppConfig.cs
+++ ThreatModellingGame.Web/App_Start/AppConfig.cs
@@ -4,11 +4,6 @@ namespace ThreatModellingGame.Web
 {
     public static class AppConfig
     {
-        public static string ApplicationName
-        {
-            get { return ConfigurationManager.AppSettings["Application_Name"]; }
-        }
-
         public static bool IsProductionEnvironment
         {
             get { return bool.Parse(ConfigurationManager.AppSettings["Is_Production_Environment"]); }
